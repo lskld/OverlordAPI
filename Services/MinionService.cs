b95f3c3e48e0eaf4217cc2c7b8de3c4a2e08a186
lskld <theo.lillieskold@gmail.com>
2026-01-15 18:10:00 +0100

    Created Minion Service class

---
+++ Services/MinionService.cs
@@ -0,0 +1,30 @@
+﻿using OverlordAPI.Interfaces;
+using OverlordAPI.Models.Entities;
+
+namespace OverlordAPI.Services
+{
+    public class MinionService : IMinionService
+    {
+        private readonly IMinionRepository _repository;
+
+        public MinionService(IMinionRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IEnumerable<Minion>> GetAllMinionsAsync()
+        {
+            return await _repository.GetAllAsync();
+        }
+
+        public async Task<bool> CreateMinionAsync(Minion minion)
+        {
+            if (minion.EvilLevel < 1 || minion.EvilLevel > 10)
+                return false;
+
+            await _repository.AddAsync(minion);
+            await _repository.SaveAsync();
+            return true;
+        }
+    }
+}
