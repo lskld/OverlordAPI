d92e496ce61d0ccdfb4e6f7cbb9ded52f1d9dd1d
lskld <theo.lillieskold@gmail.com>
2026-01-15 18:17:12 +0100

    Create MissionRepository and Inferface

---
+++ Repositories/MissionRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OverlordAPI.Data;
+using OverlordAPI.Interfaces;
+using OverlordAPI.Models.Entities;
+
+namespace OverlordAPI.Repositories
+{
+    public class MissionRepository : IMissionRepository
+    {
+        private readonly OverlordDbContext _context;
+
+        public MissionRepository(OverlordDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Mission>> GetAllAsync()
+        {
+            return await _context.Missions.Include(m => m.Minions).ToListAsync();
+        }
+
+        public async Task<Mission?> GetByIdAsync(int id)
+        {
+            return await _context.Missions.Include(m => m.Minions).FirstOrDefaultAsync(m => m.Id == id);
+        }
+        public async Task AddAsync(Mission mission)
+        {
+            await _context.Missions.AddAsync(mission); 
+        }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
