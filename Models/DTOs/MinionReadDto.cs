dbc579dbb25580b32c465feeacb12b010051bbc7
lskld <theo.lillieskold@gmail.com>
2026-01-15 18:33:20 +0100

    Create Dtos for Creating and Reading a minion

---
+++ Models/DTOs/MinionReadDto.cs
@@ -0,0 +1,10 @@
+﻿namespace OverlordAPI.Models.DTOs
+{
+    public class MinionReadDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Type { get; set; } = string.Empty; // Här kan vi skicka en sträng istället för enum-siffran
+        public string LairName { get; set; } = string.Empty;
+    }
+}
