02095a8e2d61c0950dbbf994dfa002e6d06d9efc
lskld <theo.lillieskold@gmail.com>
2026-01-15 18:06:29 +0100

    Created interfaces for mission and minion service classes

---
+++ Interfaces/IMinionService.cs
@@ -0,0 +1,10 @@
+﻿using OverlordAPI.Models.Entities;
+
+namespace OverlordAPI.Interfaces
+{
+    public interface IMinionService
+    {
+        Task<IEnumerable<Minion>> GetAllMinionsAsync();
+        Task<bool> CreateMinionAsync(Minion minion);
+    }
+}
---
+++ Interfaces/IMissionService.cs
@@ -0,0 +1,9 @@
+﻿using OverlordAPI.Models.Entities;
+
+namespace OverlordAPI.Interfaces
+{
+    public interface IMissionService
+    {
+        Task<bool> CreateMissionAsync(Mission mission);
+    }
+}
