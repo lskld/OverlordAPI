00807c7b71d34edb920e1d913beffd1629b2db97
lskld <theo.lillieskold@gmail.com>
2026-01-15 18:20:02 +0100

    Completed MissionService

---
+++ Services/MissionService.cs
@@ -0,0 +1,24 @@
+﻿using OverlordAPI.Interfaces;
+using OverlordAPI.Models.Entities;
+
+namespace OverlordAPI.Services
+{
+    public class MissionService : IMissionService
+    {
+        private readonly IMissionRepository _repository;
+
+        public MissionService(IMissionRepository repository)
+        {
+            _repository = repository;
+        }
+        public async Task<bool> CreateMissionAsync(Mission mission)
+        {
+            if (mission.Difficulty < 1 || mission.Difficulty > 10)
+                return false;
+
+            await _repository.AddAsync(mission);
+            await _repository.SaveAsync();
+            return true;
+        }
+    }
+}
